dcdb48e60a4962b6063e1889c67ee42748aaeb79
yuanfei <yuanfei_1984@hotmail.com>
2018-01-05 08:42:46 +0800

    no message

---
+++ leetcode_java/src/39.combination-sum.java
@@ -0,0 +1,48 @@
+/*
+ * [39] Combination Sum
+ *
+ * https://leetcode.com/problems/combination-sum
+ *
+ * algorithms
+ * Medium (40.07%)
+ * Total Accepted:    191.5K
+ * Total Submissions: 477.4K
+ * Testcase Example:  '[2,3,6,7]\n7'
+ *
+ * 
+ * Given a set of candidate numbers (C) (without duplicates) and a target
+ * number (T), find all unique combinations in C where the candidate numbers
+ * sums to T. 
+ * 
+ * 
+ * The same repeated number may be chosen from C unlimited number of times.
+ * 
+ * 
+ * Note:
+ * 
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ * 
+ * 
+ * 
+ * 
+ * For example, given candidate set [2, 3, 6, 7] and target 7, 
+ * A solution set is: 
+ * 
+ * [
+ * ⁠ [7],
+ * ⁠ [2, 2, 3]
+ * ]
+ * 
+ * 
+ */
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Map<Integer, List<List>> map = new HashMap<Integer, List<List>>();
+        for (int candidate : candidates) {
+        	 	
+        } 
+    }
+}
