d2c171b022d8feb551b251f07a79dd4abedf56e5
yuan-fei <yuanfei_1984@hotmail.com>
2020-05-25 10:28:04 +0800

    LC weekly 20200525

---
+++ leetcode_java/src/1457.pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode id=1457 lang=java
+ *
+ * [1457] Pseudo-Palindromic Paths in a Binary Tree
+ *
+ * https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/
+ *
+ * algorithms
+ * Medium (65.87%)
+ * Likes:    78
+ * Dislikes: 4
+ * Total Accepted:    8.3K
+ * Total Submissions: 12.6K
+ * Testcase Example:  '[2,3,1,3,1,null,1]'
+ *
+ * Given a binary tree where node values are digits from 1 to 9. A path in the
+ * binary tree is said to be pseudo-palindromic if at least one permutation of
+ * the node values in the path is a palindrome.
+ * 
+ * Return the number of pseudo-palindromic paths going from the root node to
+ * leaf nodes.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [2,3,1,3,1,null,1]
+ * Output: 2 
+ * Explanation: The figure above represents the given binary tree. There are
+ * three paths going from the root node to leaf nodes: the red path [2,3,3],
+ * the green path [2,1,1], and the path [2,3,1]. Among these paths only red
+ * path and green path are pseudo-palindromic paths since the red path [2,3,3]
+ * can be rearranged in [3,2,3] (palindrome) and the green path [2,1,1] can be
+ * rearranged in [1,2,1] (palindrome).
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [2,1,1,1,3,null,null,null,null,null,1]
+ * Output: 1 
+ * Explanation: The figure above represents the given binary tree. There are
+ * three paths going from the root node to leaf nodes: the green path [2,1,1],
+ * the path [2,1,3,1], and the path [2,1]. Among these paths only the green
+ * path is pseudo-palindromic since [2,1,1] can be rearranged in [1,2,1]
+ * (palindrome).
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [9]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The given binary tree will have between 1 and 10^5 nodes.
+ * Node values are digits from 1 to 9.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pseudoPalindromicPaths (TreeNode root) {
+        if(root == null){
+        	return 0;
+        }
+        return dfs(root, new int[10]);
+    }
+
+    int dfs(TreeNode root, int[] cnt){
+    	if(root == null){
+        	return 0;
+        }
+    	int r = 0;
+    	cnt[root.val]++;
+    	if(root.left == null && root.right == null){
+    		int odd = 0;
+    		for(int i = 0; i < cnt.length; i++){
+    			if(cnt[i] % 2 == 1){
+    				odd++;
+    			}
+    		}
+    		r = odd > 1 ? 0 : 1;
+    	}
+    	else{
+	    	r = dfs(root.left, cnt) + dfs(root.right, cnt);
+    	}
+    	cnt[root.val]--;	
+    	return r;
+    }
+}
+// @lc code=end
