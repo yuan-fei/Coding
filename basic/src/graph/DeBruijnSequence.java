adf86d3c9e0b279292d59b4e44d1fea1a6ed20d3
yuan-fei <yuanfei_1984@hotmail.com>
2022-12-03 22:59:38 +0800

    add ring and fix corner case

--- basic/src/graph/DeBruijnSequence.java
+++ basic/src/graph/DeBruijnSequence.java
@@ -10,19 +10,36 @@ package graph;
 public class DeBruijnSequence {
 
 	public static void main(String[] args) {
+		System.out.println(getRing(2, 2));
+		System.out.println(getRing(3, 2));
+		System.out.println(getRing(2, 3));
+		System.out.println(getRing(3, 1));
 		System.out.println(getSequence(2, 2));
 		System.out.println(getSequence(3, 2));
 		System.out.println(getSequence(2, 3));
+		System.out.println(getSequence(3, 1));
 	}
 
 	/*
 	 * Find Euler circle in graph with k^(n - 1) nodes each has k edge
 	 */
-	public static String getSequence(int n, int k) {
-		StringBuilder sb = new StringBuilder();
+	public static String getRing(int n, int k) {
+		String prefix = "";
+		for (int i = 0; i < n - 1; i++) {
+			prefix += '0';
+		}
+		StringBuilder sb = new StringBuilder(prefix);
 		int base = (int) Math.pow(k, n - 1);
 		euler(0, n, k, new boolean[base][k], sb);
 		return sb.toString();
+	}
+
+	public static String getSequence(int n, int k) {
+		String s = getRing(n, k);
+		if (k > 1) {
+			s += s.substring(0, n - 1);
+		}
+		return s;
 	}
 
 	static void euler(int u, int n, int k, boolean[][] seen, StringBuilder sb) {
